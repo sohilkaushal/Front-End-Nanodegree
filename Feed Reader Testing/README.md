4b0874d7b3b1c9e277a43a70abd214190d76f184
Sohil Kaushal <sohilkaushal@gmail.com>
2017-08-19 18:30:32 +0530

    Initial Commit
    Feeder Reader initial commit

---
+++ Feed Reader Testing/README.md
@@ -0,0 +1,24 @@
+# Feed Reader Testing Project
+
+### Overview of the project
+
+* In this project we are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite.
+* In this many new tests suits were also build in.
+
+### Requirements
+* Web Browser of your choice.
+* IDE of your choice to gothrough the code if you like.
+
+### How to operate the given application
+
+* Download the Feed Reader Testing.zip file.
+* Unzip the file and open the folder, now locate the index.html file.
+* Open index.html file in any browser of your choice.
+* Wait for a moment to complete the processing to complete.
+* In my case the wating period was around 4-5 seconds.
+
+####Credits
+* Udacity Forum
+* Stack Overflow
+* Random Google Searches
+* Some Random Projects on github that I consulted from time to time.
---
+++ Feed Reader Testing/jasmine/spec/feedreader.js
@@ -0,0 +1,117 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+    /* This is our first test suite - a test suite just contains
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+        /*  Write a test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
+        it('has a defined and not null url', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length > 0).toBe(true);
+            }
+
+        });
+
+
+
+
+        /*  Write a test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+        it('is not null and has name', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length > 0).toBe(true);
+            }
+        });
+    });
+
+
+
+
+    /* Write a new test suite named "The menu" */
+    describe('The Menu', function() {
+        //checking if the menu elements are hidden or not//
+        it('check if menu elements have hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+        it('display or hide menu on click', function() {
+            $('.menu-icon-link').click(); //click will hide
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            $('.menu-icon-link').click(); //click will show
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
+
+
+
+    /* Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+        it('Has a Single Entry', function(done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+            done();
+        });
+    });
+
+    describe('new feed', function() {
+
+        var feed;
+        var new_feed;
+        beforeEach(function(done) {
+
+            loadFeed(0, function() { //loading before testing
+                feed = $('.feed').html();
+
+                loadFeed(1, function() { //meeting asynch requiremnents
+                    new_feed = $('.feed').html();
+                    done();
+
+                });
+            });
+
+        });
+
+        it('feed change', function(done) {
+            var feed = $('.feed').html();
+            expect(feed).not.toEqual(new_feed);
+
+            // check for old and new feed to be different
+        });
+    });
+
+
+
+
+}());
